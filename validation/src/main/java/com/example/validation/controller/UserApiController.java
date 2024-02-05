6c3a9139faf6e971c18d8128d5fb20b91d4d68eb
권주리 <rnjswnfl99@gmail.com>
2024-02-05 20:34:00 +0900

    Valid 및 BingdingResult

--- validation/src/main/java/com/example/validation/controller/UserApiController.java
+++ validation/src/main/java/com/example/validation/controller/UserApiController.java
@@ -6,10 +6,13 @@ import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -31,14 +34,39 @@ public class UserApiController {
     }*/
 
     //[방법 2]; Api의 스펙을 통해서만 json을 보낼 수 있음
+    /*
+    [BindingResult]
+    해당 valid가 실행 시, 해당 결과를 bindingresult에 당ㅁ아줌
+    bindingresult: 에러 코드와 에러 카운트를 알 수 있음
+     */
     @PostMapping("")
-    public Api<UserRegisterRequest> requestApi(
+    public Api<? extends Object> requestApi(
             @Valid
             @RequestBody
-            Api<UserRegisterRequest> userRegisterRequest//Body를 Api 형태로 보내야만 함
-    ) {
-        log.info("init:{}", userRegisterRequest);
+            Api<UserRegisterRequest> userRegisterRequest,//Body를 Api 형태로 보내야만 함
 
+            BindingResult bindingResult //해당 valid가 실행 시, 해당 결과를 bindingresult에 담아줌
+    ) {
+       log.info("init:{}", userRegisterRequest);
+
+       if(bindingResult.hasErrors()) {//에러가 있는가?
+           var errMsgResult=bindingResult.getFieldErrors().stream()
+                   .map(it->{
+                       var format="%s :{%s}은 %s";
+                       var msg=String.format(format,it.getField(),it.getRejectedValue(),it.getDefaultMessage());
+                       return msg;
+                   }).collect(Collectors.toList());
+           var err=Api.Error.builder()
+                   .errorMsg(errMsgResult)
+                   .build();
+
+           var errResponse=Api.builder()
+                   .resultCode(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+                   .resultMsg(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                   .error(err)
+                   .build();
+           return errResponse;//body가 없어 에러,함수의 반환 타입 수정 필요 Api<UserRegisterRequest> -> Api<Object>
+       }
         //request의 echo 방식
         var body = userRegisterRequest.getData();
         Api<UserRegisterRequest> response = Api.<UserRegisterRequest>builder()
