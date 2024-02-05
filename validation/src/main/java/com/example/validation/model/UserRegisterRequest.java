290ce95d154367bf1186544fa5c035c046b00fca
권주리 <rnjswnfl99@gmail.com>
2024-02-05 21:46:16 +0900

    Valid 시, 복합(중복)적인 조건일 때 메서드 사용 AssertTrue(@) 사용함

--- validation/src/main/java/com/example/validation/model/UserRegisterRequest.java
+++ validation/src/main/java/com/example/validation/model/UserRegisterRequest.java
@@ -8,6 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Data
 @AllArgsConstructor
@@ -17,9 +18,11 @@ public class UserRegisterRequest {
 
    // @NotNull //!=null
    // @NotEmpty //!=null && !="", 공백도 아님
-    @NotBlank //!=null && !="" && !=" " //blank가 있는 문자도 안됨
+    //@NotBlank //!=null && !="" && !=" " //blank가 있는 문자도 안됨
     private String name;
+//[1] validation 시, 중복 조건(name과 nickName 듈 중 하나만 있으면 됨)
 
+    private String nickName;
     @Size(min=1, max=12)
     @NotBlank
     private String password;//pw는 blank도 안되며 길이는 1~12
@@ -38,4 +41,16 @@ public class UserRegisterRequest {
 
    @FutureOrPresent//오늘(현재) 이상을 뜻 함
     private LocalDateTime registerAt;
+
+
+   @AssertTrue(message = "name or nickName은 존재해야 합니다.")//해당 return 값이 true가 아니면 통과할 수 없는 메서드
+   public boolean isnameCheck(){//해당 메서드가 반드시 is로 시작해야 함
+    if(Objects.nonNull(name) && !name.isBlank())//name이 null이 아니고 blank가 아니면 통과
+     return true;
+
+    if(Objects.nonNull(nickName) && !nickName.isBlank())//not null 먼저 체크를 해줘야 함
+     return true;
+
+   return false;
+   }
 }
