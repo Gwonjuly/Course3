de59fe33af70113f261e934c3abbc3712c1de2d3
권주리 <rnjswnfl99@gmail.com>
2024-02-14 00:09:19 +0900

    memoryDB의 BookService 작성 완료

--- memoryDB merge with JPA/src/main/java/com/example/memoryDB/book/service/BookService.java
+++ memoryDB merge with JPA/src/main/java/com/example/memoryDB/book/service/BookService.java
@@ -1,15 +1,33 @@
 package com.example.memoryDB.book.service;
 
+import com.example.memoryDB.book.db.entity.BookEntity;
 import com.example.memoryDB.book.db.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 
 public class BookService {
 
-    @Autowired
-    private BookRepository bookRepository;
+
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository){
+        this.bookRepository=bookRepository;
+    }
+    //create, updates
+    public BookEntity create(BookEntity book){
+        return bookRepository.save(book);
+    }
+    //all
+    public List<BookEntity> findAll(){
+        return bookRepository.findAll();
+    }
+    //delete
+
+    //find one
 
 }
