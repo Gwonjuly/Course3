c8ae624f672a9368f28f1f0523a2e59ff7621797
권주리 <rnjswnfl99@gmail.com>
2024-02-13 23:50:39 +0900

    memoryDB의 BookRepository(이름 바뀜ㅎ)

--- memoryDB merge with JPA/src/main/java/com/example/memoryDB/book/db/entity/BookEntity.java
+++ memoryDB merge with JPA/src/main/java/com/example/memoryDB/book/db/entity/BookEntity.java
@@ -1,4 +1,18 @@
 package com.example.memoryDB.book.db.entity;
 
-public class BookEntity {
+import com.example.memoryDB.Entity.Entity;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BookEntity extends Entity {
+
+    private String name;
+    private String category;
+    private BigDecimal amount;
 }
--- memoryDB merge with JPA/src/main/java/com/example/memoryDB/user/db/UserRepository.java
+++ memoryDB merge with JPA/src/main/java/com/example/memoryDB/user/db/UserRepository.java
@@ -3,6 +3,6 @@ package com.example.memoryDB.user.db;
 import com.example.memoryDB.user.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface UserRepository extends JpaRepository<UserEntity,Long> {
+public interface UserRepository extends JpaRepository<UserEntity,Long> {
 
 }
