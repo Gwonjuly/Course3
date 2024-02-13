66910309e6a9a4604c01cf660dcc65b9320a8874
권주리 <rnjswnfl99@gmail.com>
2024-02-13 23:55:39 +0900

    memoryDB의 BookEntity의 jpa 변환

--- memoryDB merge with JPA/src/main/java/com/example/memoryDB/book/db/entity/BookEntity.java
+++ memoryDB merge with JPA/src/main/java/com/example/memoryDB/book/db/entity/BookEntity.java
@@ -1,16 +1,23 @@
 package com.example.memoryDB.book.db.entity;
 
-import com.example.memoryDB.Entity.Entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
 
 import java.math.BigDecimal;
 
-@EqualsAndHashCode(callSuper = true)
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class BookEntity extends Entity {
+@Entity(name = "book")
+public class BookEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String name;
     private String category;
