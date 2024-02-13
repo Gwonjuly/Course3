3af0171f29d1f2648cbdd3ae6e4151cbf55ce17e
권주리 <rnjswnfl99@gmail.com>
2024-02-14 00:06:37 +0900

    memoryDB의 BookService의 빈 주입 방법 3 Autowired

--- memoryDB merge with JPA/src/main/java/com/example/memoryDB/book/service/BookService.java
+++ memoryDB merge with JPA/src/main/java/com/example/memoryDB/book/service/BookService.java
@@ -2,15 +2,14 @@ package com.example.memoryDB.book.service;
 
 import com.example.memoryDB.book.db.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
+
 public class BookService {
 
-    private final BookRepository bookRepository;
+    @Autowired
+    private BookRepository bookRepository;
 
-    /*public BookService(BookRepository bookRepository){//BookService 생 성자를 통해 bookservice가 생성될 때 bookrepository 주입
-      this.bookRepository=bookRepository;//bookrepository는 서비스로 등록되어 있음
-    }//RequiredArgsConstructor 있으면 위 코드 없어도 됨*/
 }
