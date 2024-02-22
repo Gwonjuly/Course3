2a2d5aea93d5c263e80e6b7e394062f47d7da242
권주리 <rnjswnfl99@gmail.com>
2024-02-23 00:32:47 +0900

    메서드 실행 전/후 시점 캐치: UserRequest로 들어온 값을 메서드 실행 전에 변경함

--- filter/src/main/java/com/example/filter/aop/TimerAop.java
+++ filter/src/main/java/com/example/filter/aop/TimerAop.java
@@ -1,10 +1,14 @@
 package com.example.filter.aop;
 
+import com.example.filter.model.UserRequest;
+import org.apache.catalina.User;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect//AOP를 정의하는 클래스에 할당됨
 @Component//스프링으로부터 관리 됨
@@ -18,7 +22,20 @@ public class TimerAop {
     public void around(ProceedingJoinPoint joinPoint) throws Throwable {//pointcut을 지정한 위치
 
         System.out.println("메서드 실행 이전");
-        joinPoint.proceed();//메서드를 실행시키는 실질적인 메서드, 예외처리  필요
+        Arrays.stream(joinPoint.getArgs()).forEach(
+                it->{
+                    if(it instanceof UserRequest){
+                        var tempUser=(UserRequest)it;
+                        tempUser.setPhoneNumber(tempUser.getPhoneNumber().replace("-",""));//들어온 번호에서 대시 삭제
+                    }
+                }
+        );
+        var newObj=Arrays.asList(new UserRequest());//배열도 설정 가능함, 임시로 null 값 넣음
+
+        //.getArgs(): 해당 메서드가 실행될 때 들어가는 모든 매개변수를 불러오는 메서드
+
+        joinPoint.proceed(newObj.toArray());//메서드를 실행시키는 실질적인 메서드, 예외처리  필요
+
         System.out.println("메서드 실행 이후");
     }
 }
